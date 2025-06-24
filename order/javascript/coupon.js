b1993759383378589762904881ad73ac26535b9b
CS-Ng19 <csng1911@gmail.com>
2025-06-24 21:35:49 +0800

    Update coupon.js

--- order/javascript/coupon.js
+++ order/javascript/coupon.js
@@ -1,10 +1,10 @@
 
 window.coupons = {
 
-    sbg4: {
-        type: "flat",
-        value: 6,
+    happysummer: {
+        type: "percent",
+        value: 12.5,
         target: "VIP", "VIP+", "MVP", "MVP+", "升級至 [VIP+]", "升級至 [MVP]","升級至 [MVP+]",
-        desc: "購買 3600 Skyblock Gems 減 $4"
+        desc: "購買 Rank 減 12.5%"
     }
 };
