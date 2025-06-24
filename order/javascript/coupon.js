38a185128523aef76968430c9d7bbac9e80c7923
CS-Ng19 <csng1911@gmail.com>
2025-06-24 21:59:57 +0800

    Update coupon.js

--- order/javascript/coupon.js
+++ order/javascript/coupon.js
@@ -4,7 +4,9 @@ window.coupons = {
     happysummer: {
         type: "percent",
         value: 12.5,
-        target: "VIP", "VIP+", "MVP", "MVP+", "升級至 [VIP+]", "升級至 [MVP]","升級至 [MVP+]",
+        target: {
+            "VIP", "VIP+", "MVP", "MVP+", "升級至 [VIP+]", "升級至 [MVP]","升級至 [MVP+]"
+        },
         desc: "購買 Rank 減 12.5%"
     }
 };
