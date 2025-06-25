b208b8b05fbcd7c14212a79787b087e8f30fc5e1
CS-Ng19 <csng1911@gmail.com>
2025-06-25 18:42:50 +0800

    Update coupon.js

--- order/javascript/coupon.js
+++ order/javascript/coupon.js
@@ -1,12 +1,9 @@
-
 window.coupons = {
+    ds56387f: {
+        type: "flat",
+        threshold: 1,
+        value: 3,
+        desc: "[5星] 任何 Hypixel 產品 $3 折扣 - #56387 HuoGuangX  "
+    },
 
-    happysummer: {
-        type: "percent",
-        value: 12.5,
-        target: {
-            "VIP", "VIP+", "MVP", "MVP+", "升級至 [VIP+]", "升級至 [MVP]","升級至 [MVP+]"
-        },
-        desc: "購買 Rank 減 12.5%"
-    }
 };
