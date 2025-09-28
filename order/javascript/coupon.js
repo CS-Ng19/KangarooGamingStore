b297c8d84476abd39cb901bdb60c522867b8fef2
CS-Ng19 <csng1911@gmail.com>
2025-09-28 23:17:29 +0800

    Update coupon.js

--- order/javascript/coupon.js
+++ order/javascript/coupon.js
@@ -5,10 +5,22 @@ window.coupons = {
         value: 3,
         desc: "[5星] $3 折扣 - #56387 HuoGuangX  "
     },
-    happySummer: {
+    enjoyVIP: {
         type: "percent",
-        threshold: 1,
         value: 5,
-        desc: "[六月 & 七月 限時95折]"
+        target:"VIP"
+        desc: "[VIP Rank 限時95折]"
+    },
+    Experience: {
+        type: "flat",
+        threshold: 1,
+        value: 300,
+        desc: "[選購任何產品加配 Experience Cape 即減$300]"
+    },
+    Oct3: {
+        type: "flat",
+        threshold: 99,
+        value: 3,
+        desc: "滿$99減$3"
     }
 };
