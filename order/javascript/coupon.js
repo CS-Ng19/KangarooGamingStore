151649f0db6d9d80a1c379eb70f0fa1739a36268
CS-Ng19 <csng1911@gmail.com>
2025-11-01 22:19:16 +0800

    Update coupon.js

--- order/javascript/coupon.js
+++ order/javascript/coupon.js
@@ -1,10 +1,5 @@
 window.coupons = {
-    ds56387f: {
-        type: "flat",
-        threshold: 1,
-        value: 3,
-        desc: "[5星] $3 折扣 - #56387 HuoGuangX  "
-    },
+   
     enjoyVIP: {
         type: "percent",
         value: 5,
@@ -17,11 +12,17 @@ window.coupons = {
         value: 300,
         desc: "選購任何產品加配 Experience Cape 即減$300"
     },
-    Oct3: {
+    Nov3: {
         type: "flat",
         threshold: 99,
         value: 3,
         desc: "滿$99減$3"
+    },
+    xmas7: {
+        type: "flat",
+        threshold: 99,
+        value: 7,
+        desc: "滿$99減$7"
     },
     Halloween25: {
         type: "percent",
